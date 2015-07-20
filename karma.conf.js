6f6121460234aada412c7c886fa2d73f15cfc3c0
Remon Oldenbeuving <remon@hackerone.com>
2015-07-20 15:19:33 +0200

    Removed tabs from karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -4,86 +4,86 @@ var webpackConfig = require('./webpack.config');
 module.exports = function(config) {
   config.set({
 
-	// base path, that will be used to resolve files and exclude
-	basePath: '',
+  // base path, that will be used to resolve files and exclude
+  basePath: '',
 
 
-	// frameworks to use
-	frameworks: ['mocha', 'chai', 'sinon'],
+  // frameworks to use
+  frameworks: ['mocha', 'chai', 'sinon'],
 
 
-	// list of files / patterns to load in the browser
-	files: [
-	  'test/index.coffee'
-	],
+  // list of files / patterns to load in the browser
+  files: [
+    'test/index.coffee'
+  ],
 
 
-	// list of preprocessors
-	preprocessors: {
-	  'test/*': ['webpack']
-	},
+  // list of preprocessors
+  preprocessors: {
+    'test/*': ['webpack']
+  },
 
 
-	webpack: webpackConfig,
+  webpack: webpackConfig,
 
 
-	webpackMiddleware: {
+  webpackMiddleware: {
     noInfo: true
-	},
+  },
 
 
-	// test results reporter to use
-	// possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-	reporters: ['progress'],
+  // test results reporter to use
+  // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+  reporters: ['progress'],
 
 
-	// web server port
-	port: 9876,
+  // web server port
+  port: 9876,
 
 
-	// enable / disable colors in the output (reporters and logs)
-	colors: true,
+  // enable / disable colors in the output (reporters and logs)
+  colors: true,
 
 
-	// level of logging
-	// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-	logLevel: config.LOG_INFO,
+  // level of logging
+  // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+  logLevel: config.LOG_INFO,
 
 
-	// enable / disable watching file and executing tests whenever any file changes
-	autoWatch: true,
+  // enable / disable watching file and executing tests whenever any file changes
+  autoWatch: true,
 
 
-	// Start these browsers, currently available:
-	// - Chrome
-	// - ChromeCanary
-	// - Firefox
-	// - Opera (has to be installed with `npm install karma-opera-launcher`)
-	// - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
-	// - PhantomJS
-	// - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-	browsers: ['PhantomJS'],
+  // Start these browsers, currently available:
+  // - Chrome
+  // - ChromeCanary
+  // - Firefox
+  // - Opera (has to be installed with `npm install karma-opera-launcher`)
+  // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+  // - PhantomJS
+  // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+  browsers: ['PhantomJS'],
 
 
-	// If browser does not capture in given timeout [ms], kill it
-	captureTimeout: 60000,
+  // If browser does not capture in given timeout [ms], kill it
+  captureTimeout: 60000,
 
 
-	// Continuous Integration mode
-	// if true, it capture browsers, run tests and exit
-	singleRun: false,
+  // Continuous Integration mode
+  // if true, it capture browsers, run tests and exit
+  singleRun: false,
 
 
-	// List plugins explicitly, since autoloading karma-webpack
-	// won't work here
-	plugins: [
-		require("karma-mocha"),
+  // List plugins explicitly, since autoloading karma-webpack
+  // won't work here
+  plugins: [
+    require("karma-mocha"),
     require("karma-chai"),
-		require("karma-phantomjs-launcher"),
-		require("karma-chrome-launcher"),
-		require("karma-webpack"),
+    require("karma-phantomjs-launcher"),
+    require("karma-chrome-launcher"),
+    require("karma-webpack"),
     require("karma-sinon"),
     require("karma-json-reporter")
-	]
+  ]
   });
 };
